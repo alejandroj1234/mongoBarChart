abec3de4262f60937f15102acda9eb0e0f85143b
Alejandro <alejandroj1234@gmail.com>
2015-12-28 01:35:17 -0500

    changing port stuff

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(process.env.PORT || 5000)
+app.use(express.static(__dirname + "/public/"));
+app.listen(process.env.PORT || 5000);
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
